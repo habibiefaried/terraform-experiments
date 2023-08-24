d68292664e816b42eb14881f980e1f1beae8c420
Habibie Faried <habibiefaried@gmail.com>
2023-08-25 05:34:10 +0700

    feat: modulize

--- network-firewall-ingress/config.tf
+++ network-firewall-ingress/config.tf
@@ -27,8 +27,14 @@ terraform {
 }
 
 module "firewall_ingress" {
-  source  = "./firewall-ingress"
+  source  = "../modules/firewall-ingress"
   vpc     = aws_vpc.main
   subnets = local.subnets
   aws_igw = aws_internet_gateway.gw
+}
+
+module "ec2" {
+  source    = "../modules/ec2"
+  subnet_id = aws_subnet.public_subnets["ap-southeast-3a"].id
+  vpc_id = aws_vpc.main.id
 }
