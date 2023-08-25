f6f3b9e50208018d7465d7a0b91aa94c55922f0d
Habibie Faried <habibiefaried@gmail.com>
2023-08-26 05:24:48 +0700

    feat: using iam ip

--- modules/ec2/instance.tf
+++ modules/ec2/instance.tf
@@ -10,5 +10,5 @@ module "ec2_instance" {
   subnet_id                   = var.subnet_id
   associate_public_ip_address = true
   availability_zone           = "ap-southeast-3a"
-  iam_instance_profile        = var.iam_instance_profile
+  iam_instance_profile        = aws_iam_instance_profile.ec2.name
 }
